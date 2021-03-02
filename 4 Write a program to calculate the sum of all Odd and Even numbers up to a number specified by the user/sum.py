27a21f9154552a0d7de897179edc34e21683fc77
PARVATHY-S-PRAKASH <79054194+PARVATHY-S-PRAKASH@users.noreply.github.com>
2021-03-02 21:40:00 +0530

    Add files via upload

---
+++ 4 Write a program to calculate the sum of all Odd and Even numbers up to a number specified by the user/sum.py
@@ -0,0 +1,22 @@
+
+n=int(input("enter the maximum value: "))
+
+evenSum=0
+
+oddSum=0
+
+
+for num in range(1,n+1):
+    
+   if (num%2==0):
+        
+      evenSum=evenSum+num
+    
+   else:
+        
+      oddSum=oddSum+num
+
+
+print("The sum of even numbers = " ,evenSum)
+
+print("The sum of odd numbers  = " ,oddSum)
---
+++ 5 Evaluate the area and perimeter of a circle using functions from user input/circle.py
@@ -0,0 +1,29 @@
+
+import math
+
+
+
+def Area(radius):
+    
+	return math.pi * radius * radius
+ 
+   
+
+def Perimeter(radius):
+    
+	return 2 * math.pi * radius
+
+
+
+
+r = int(input(' enter the radius of a circle: '))
+
+area = Area(r)
+perimeter = Perimeter(r)
+
+
+
+
+print("\n Area Of a Circle =" ,area)
+print(" Perimeter Of a Circle =" ,perimeter)
+
